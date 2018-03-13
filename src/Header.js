5066c9ead8256f929684e3f49246d0be87e58249
codeofrich <codeofrich@gmail.com>
2018-03-13 17:52:54 +0700

    adding header logo

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Logo from './Logo';
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <Logo />
+            </div>
+        );
+    }
+}
+
+export default Header;
---
+++ src/Logo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Logo.css';
+
+class Logo extends Component {
+    render() {
+        return (
+            <div>
+                <h1>OmniFood</h1>
+            </div>
+        );
+    }
+}
+
+export default Logo;
