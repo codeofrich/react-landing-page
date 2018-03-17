889653bfaedcf03b6c576dafddc528087e10e837
codeofrich <codeofrich@gmail.com>
2018-03-17 23:23:18 +0700

    adding navigation links

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Logo from './Logo';
+import Navigation from './Navigation';
 
 class Header extends Component {
     render() {
         return (
             <header>
                 <Logo />
+                <Navigation />
             </header>
         );
     }
---
+++ src/Navigation.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Navigation extends Component {
+    render() {
+        return (
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>Products</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Navigation;
